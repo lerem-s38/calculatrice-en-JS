7c5e76bb12109a54a6edfc432e65c7d70311da3e
Rémi CUSSET <remicusset72@gmail.com>
2020-03-04 16:42:19 +0100

    partie2

--- calculatrice.js
+++ calculatrice.js
@@ -1,6 +1,4 @@
 let choix = ('Que souhaitez vous faire?') ;
-let add   = 1 , mult  = 2 , soust = 3 , div   = 4 ;
-
 
 
 for (let choix = 1; choix < 5; choix++) {
@@ -42,9 +40,38 @@ for (let choix = 1; choix < 5; choix++) {
                   
 }
 
-  
-     choix =  prompt(' Que souhaitez-vous faire ?\n\n1 - Addition\n2 - Multiplication\n3 - Soustraction\n4 - Division\n.')
+
+
+     choix =  prompt(' Que souhaitez-vous faire ?\n\n1 - Addition\n2 - Multiplication\n3 - Soustraction\n4 - Division\n') ;
+
     // je veux afficher si le choix n'est pas 1 ,2, 3,4 erreur('Veuillez faire un choix entre 1 & 4')
-   if (choix > 4  ) {
+    // je veux afficher erreur ce n'est pas un nombre.
+     if (choix > 4  ) {
        alert('Erreur veuillez choisir entre 1 et 4') ;
+       
+       
    }
+   
+   
+        //let NombreA = (' Entrer votre premier nombre') ;
+        //let NombreB = ('Entrer votre deuxiéme nombre') ;
+
+        do {
+
+            var NombreA  = prompt(' Entrer votre premier nombre') ;
+        
+        } while(NombreA == "" || NombreA == null ) ;
+        
+        alert('Vous avez saisi : ' + NombreA); 
+
+
+
+        do {
+
+            var NombreB  = prompt(' Entrer votre deuxiéme nombre') ;
+        
+        } while(NombreB == "" || NombreB == null  ) ; 
+        
+        alert('Vous avez saisi : ' + NombreB);  
+        
+        
